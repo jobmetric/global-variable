6080b541411b1e44160da1a3113a71089779c42e
majeedmohammadian <majeedmohammadian@gmail.com>
2022-12-09 18:29:16 +0330

    add keywords and links and styles and scripts in Document object

--- src/Object/Document.php
+++ src/Object/Document.php
@@ -8,7 +8,11 @@ class Document
 
     private string $title = '';
     private string $description = '';
+    private string $keywords = '';
     private ?string $canonical = null;
+    private array $links = [];
+    private array $styles = [];
+    private array $scripts = [];
 
     /**
      * get instance object
@@ -69,6 +73,28 @@ class Document
     }
 
     /**
+     * set keywords page
+     *
+     * @param string $keywords
+     *
+     * @return void
+     */
+    public function setKeywords(string $keywords): void
+    {
+        $this->keywords = $keywords;
+    }
+
+    /**
+     * get keywords page
+     *
+     * @return string
+     */
+    public function getKeywords(): string
+    {
+        return $this->keywords;
+    }
+
+    /**
      * set canonical link
      *
      * @param string $url
@@ -88,5 +114,81 @@ class Document
     public function getCanonical(): ?string
     {
         return $this->canonical;
+    }
+
+    /**
+     * set link page
+     *
+     * @param string $href
+     * @param string $rel
+     *
+     * @return void
+     */
+    public function addLink(string $href, string $rel): void
+    {
+        $this->links[$href] = [
+            'href' => $href,
+            'rel'  => $rel
+        ];
+    }
+
+    /**
+     * get link page
+     *
+     * @return array
+     */
+    public function getLinks(): array
+    {
+        return $this->links;
+    }
+
+    /**
+     * set style page
+     *
+     * @param string $href
+     * @param string $rel
+     * @param string $media
+     *
+     * @return void
+     */
+    public function addStyle(string $href, string $rel = 'stylesheet', string $media = ''): void
+    {
+        $this->styles[md5($href)] = [
+            'href'  => $href,
+            'rel'   => $rel,
+            'media' => $media
+        ];
+    }
+
+    /**
+     * get style page
+     *
+     * @return array
+     */
+    public function getStyles(): array
+    {
+        return $this->styles;
+    }
+
+    /**
+     * set script page
+     *
+     * @param string $script
+     *
+     * @return void
+     */
+    public function addScript(string $script): void
+    {
+        $this->scripts[md5($script)] = $script;
+    }
+
+    /**
+     * get script page
+     *
+     * @return array
+     */
+    public function getScripts(): array
+    {
+        return $this->scripts;
     }
 }
