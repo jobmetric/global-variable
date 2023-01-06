3c83ba572425ea85254876c568f1c4b22d60d4a9
majeedmohammadian <majeedmohammadian@gmail.com>
2023-01-06 21:16:38 +0330

    add config function to GlobalVariableService

--- src/GlobalVariableService.php
+++ src/GlobalVariableService.php
@@ -3,16 +3,27 @@
 namespace JobMetric\GlobalVariable;
 
 use JobMetric\GlobalVariable\Object\Document;
+use JobMetric\GlobalVariable\Object\Config;
 
 class GlobalVariableService
 {
     /**
-     * get instance object
+     * get instance document object
      *
      * @return Document
      */
     public function document(): Document
     {
         return Document::getInstance();
+    }
+    
+    /**
+     * get instance config object
+     *
+     * @return Config
+     */
+    public function config(): Config
+    {
+        return Config::getInstance();
     }
 }
